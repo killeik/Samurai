6635fc98309e93032d7d36e52bd7bf3ef5dea868
Serafim <sixwingedkilleik@gmail.com>
2023-01-09 02:51:16 +0300

    Added Menu View

--- main.py
+++ main.py
@@ -3,11 +3,72 @@ Samurai Game
 """
 
 import arcade
+import arcade.gui
+
 
 # Constants
 SCREEN_WIDTH = 1366
 SCREEN_HEIGHT = 768
 SCREEN_TITLE = "Samurai"
+
+
+class MenuView(arcade.View):
+    """
+    Class that manages the menu view.
+    """
+
+    def __init__(self):
+        # Call the parent class and set up the window
+        super().__init__()
+
+        # Initialize UIManger to handle UI
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+        # Set background color
+        arcade.set_background_color(arcade.color.GRAY)
+
+        # Create a vertical BoxGroup to align buttons
+        self.v_box = arcade.gui.UIBoxLayout()
+
+        # Create the buttons
+        start_button = arcade.gui.UIFlatButton(text="Start Game", width=200)
+        self.v_box.add(start_button.with_space_around(bottom=25))
+        start_button.on_click = self.on_click_start
+
+        settings_button = arcade.gui.UIFlatButton(text="Settings", width=200)
+        self.v_box.add(settings_button.with_space_around(bottom=25))
+        settings_button.on_click = self.on_click_settings
+
+        quit_button = arcade.gui.UIFlatButton(text="Quit", width=200)
+        self.v_box.add(quit_button)
+        quit_button.on_click = self.on_click_quit
+
+        # Create a widget to hold the v_box widget, that will center the buttons
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="center_y",
+                child=self.v_box)
+        )
+
+    def on_click_start(self, event):
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+    def on_click_settings(self, event):
+        print("Settings:", event)
+
+    def on_click_quit(self, event):
+        arcade.exit()
+
+    def on_draw(self):
+        """Render the screen."""
+
+        self.clear()
+        # Code to draw the screen goes here
+        self.manager.draw()
 
 
 class GameView(arcade.View):
@@ -35,8 +96,8 @@ class GameView(arcade.View):
 def main():
     """Startup"""
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game_view = GameView()
-    window.show_view(game_view)
+    menu_view = MenuView()
+    window.show_view(menu_view)
     arcade.run()
 
 
